b2c1cc3d6e4f1f8570529f2c4103bf42dda4a98a
Lamar <lamar@growtherapy.com>
2024-12-19 14:09:04 -0600

    Solve 2024/19 part 2

--- 2024/19/python/solve.py
+++ 2024/19/python/solve.py
@@ -13,16 +13,17 @@ def _parse_file(filename: str) -> tuple[list[str], list[str]]:
 
 
 @functools.cache
-def _find_patterns(design: str) -> list[str]:
-    if design in PATTERNS:
-        return [design]
-
+def _find_patterns(design: str) -> int:
+    arrangements = 0
     for pattern in PATTERNS:
-        if design.startswith(pattern):
-            remaining = _find_patterns(design[len(pattern) :])  # noqa
-            if remaining:
-                return [pattern] + remaining
-    return []
+        if pattern == design:
+            arrangements += 1
+        elif design.startswith(pattern):
+            remaining_design = design[len(pattern) :]  # noqa
+            remaining_arrangements = _find_patterns(remaining_design)
+            arrangements += remaining_arrangements
+
+    return arrangements
 
 
 if __name__ == "__main__":
@@ -32,5 +33,6 @@ if __name__ == "__main__":
 
     PATTERNS, designs = _parse_file(args.filename)
     arrangements = {design: _find_patterns(design) for design in designs}
-    achievable_designs = [design for design, arrangement in arrangements.items() if arrangement]
+    achievable_designs = [design for design, arrangements in arrangements.items() if arrangements]
     print(f"Achievable designs #: {len(achievable_designs)}")
+    print(f"Total arrangements #: {sum(arrangements.values())}")
