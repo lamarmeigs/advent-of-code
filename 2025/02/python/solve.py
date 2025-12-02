33c6384f333b3ca0744c9c4448c26db2a26c5446
Lamar <lamar@growtherapy.com>
2025-12-02 08:54:01 -0600

    Solve 2025/02 part 2

--- 2025/02/python/solve.py
+++ 2025/02/python/solve.py
@@ -13,7 +13,7 @@ def _parse_file(filename: str) -> list[tuple[int, int]]:
     return ranges
 
 
-def _find_invalid_ids(start: int, end: int):
+def _find_invalid_ids_part_1(start: int, end: int):
     for product_id in range(start, end + 1):
         product_id = str(product_id)
         length = len(product_id)
@@ -23,6 +23,19 @@ def _find_invalid_ids(start: int, end: int):
                 yield int(product_id)
 
 
+def _find_invalid_ids_part_2(start: int, end: int):
+    for product_id in range(start, end + 1):
+        product_id = str(product_id)
+        length = len(product_id)
+        for subrange in range(1, int((length / 2) + 1)):
+            if length % subrange != 0:
+                continue
+            pattern = product_id[:subrange]
+            if product_id == pattern * int(length / len(pattern)) and pattern != product_id:
+                yield int(product_id)
+                break
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
@@ -30,7 +43,13 @@ if __name__ == "__main__":
 
     ranges = _parse_file(args.filename)
     bad_product_ids = itertools.chain.from_iterable(
-        _find_invalid_ids(start, end) for start, end in ranges
+        _find_invalid_ids_part_1(start, end) for start, end in ranges
+    )
+    result = sum(bad_product_ids)
+    print(f"Sum of invalid IDs: {result}")
+
+    bad_product_ids = itertools.chain.from_iterable(
+        _find_invalid_ids_part_2(start, end) for start, end in ranges
     )
     result = sum(bad_product_ids)
     print(f"Sum of invalid IDs: {result}")
