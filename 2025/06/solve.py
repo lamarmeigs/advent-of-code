91e8f67eaa2e5fbf5d81d129d2a93d6e24fb704b
Lamar <lamar@growtherapy.com>
2025-12-06 09:35:38 -0600

    Solve 2025/06 part 2

--- 2025/06/solve.py
+++ 2025/06/solve.py
@@ -3,17 +3,40 @@ import math
 import re
 
 
-def _parse_file(filename: str) -> tuple[list[int], list[str]]:
+def _parse_file(filename: str) -> tuple[list[str], list[str]]:
     with open(filename) as f:
         raw_lines = f.readlines()
-
     operations = raw_lines.pop().split()
+    return raw_lines, operations
+
+
+def _parse_by_group(raw_numbers: list[str]) -> list[int]:
     lines = []
-    for line in raw_lines:
+    for line in raw_numbers:
         numbers = [int(match.group()) for match in re.finditer(r"\d+", line)]
         lines.append(numbers)
     columns = list(zip(*lines))
-    return columns, operations
+    return columns
+
+
+def _parse_by_column(raw_numbers: list[str]) -> list[int]:
+    columns = []
+    column = []
+    for i in range(len(raw_numbers[0])):
+        empty_column = True
+        number = ''
+        for line in raw_numbers:
+            character = line[i]
+            if not character.isspace():
+                number += character
+                empty_column = False
+
+        if empty_column:
+            columns.append(column)
+            column = []
+        else:
+            column.append(int(number))
+    return columns
 
 
 def _do_math(numbers: list[int], operation: str) -> int:
@@ -32,6 +55,11 @@ if __name__ == "__main__":
     parser.add_argument("filename")
     args = parser.parse_args()
 
-    columns, operations = _parse_file(args.filename)
-    result = sum(_do_math(columns[i], operations[i]) for i in range(len(columns)))
+    raw_numbers, operations = _parse_file(args.filename)
+    columns = _parse_by_group(raw_numbers)
+    result = sum(_do_math(numbers, operation) for numbers, operation in zip(columns, operations))
     print(f"Sum of all individual problems: {result}")
+
+    columns = _parse_by_column(raw_numbers)
+    result = sum(_do_math(numbers, operation) for numbers, operation in zip(columns, operations))
+    print(f"Sum of all individual columnar problems: {result}")
