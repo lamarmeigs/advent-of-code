5dbad03fae0c1b077ac18df3218981f60b5df5a8
Lamar <lamar@growtherapy.com>
2025-12-13 16:24:49 -0600

    Solve 2025/11 part 2

--- 2025/11/solve.py
+++ 2025/11/solve.py
@@ -1,5 +1,5 @@
 import argparse
-import collections
+import functools
 
 
 def _parse_file(filename: str) -> dict[str, list[str]]:
@@ -14,14 +14,24 @@ def _parse_file(filename: str) -> dict[str, list[str]]:
     return devices
 
 
-def _count_paths(devices: dict[str, list[str]]) -> int:
-    paths_to = collections.defaultdict(int)
-    next_devices = ["you"]
-    while len(next_devices) > 0:
-        next_device = next_devices.pop(-1)
-        paths_to[next_device] += 1
-        next_devices.extend(devices[next_device])
-    return paths_to["out"]
+class PathFinder:
+    def __init__(self, devices):
+        self.devices = devices
+
+    @functools.cache
+    def count_paths(
+        self,
+        start: str,
+        end: str,
+        required: tuple[str] = (),
+    ) -> int:
+        if start == end:
+            return 1 if not required else 0
+        if start in required:
+            required = tuple(device for device in required if device != start)
+
+        neighbors = self.devices[start]
+        return sum(self.count_paths(neighbor, end, required) for neighbor in neighbors)
 
 
 if __name__ == "__main__":
@@ -30,5 +40,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     devices = _parse_file(args.filename)
-    paths_to_out = _count_paths(devices)
+    path_finder = PathFinder(devices)
+
+    paths_to_out = path_finder.count_paths("you", "out")
+    print(f"Number of paths to 'out': {paths_to_out}")
+
+    paths_to_out = path_finder.count_paths("svr", "out", required=("fft", "dac"))
     print(f"Number of paths to 'out': {paths_to_out}")
